7f93d7fcb87c98ba38f32554d42bdc4f78643ffe
Matija Han <matijah@ecobee.com>
2018-09-14 10:51:57 -0400

    Initial commit

---
+++ src/main/kotlin/com/ecobee/codecamp/tutorial/TutorialApplication.kt
@@ -0,0 +1,12 @@
+package com.ecobee.codecamp.tutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class TutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<TutorialApplication>(*args)
+}
