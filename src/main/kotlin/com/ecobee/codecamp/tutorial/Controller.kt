dd9b99f982439674196bd3fd17cc8dbdb2ee18c2
Matija Han <matijah@ecobee.com>
2018-09-17 09:40:46 -0400

    Removed unneeded imports

--- src/main/kotlin/com/ecobee/codecamp/tutorial/Controller.kt
+++ src/main/kotlin/com/ecobee/codecamp/tutorial/Controller.kt
@@ -2,8 +2,6 @@ package com.ecobee.codecamp.tutorial
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 
 @RestController
